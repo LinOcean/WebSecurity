8e2d9f08b02fd620104f2f31e5979db5c9624ea5
ocean <13650968987@163.com>
2021-02-21 15:14:01 +0800

    https请求

--- server/src/main/java/com/MyServerSpringBootApplication.java
+++ server/src/main/java/com/MyServerSpringBootApplication.java
@@ -2,6 +2,7 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author: ocean
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  **/
 
 @SpringBootApplication
+@ServletComponentScan(basePackages = "com.filter")
 public class MyServerSpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(MyServerSpringBootApplication.class, args);
